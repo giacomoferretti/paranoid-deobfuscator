a6f63af001650f32d404fb68e7f2b148e1c39b15
Giacomo Ferretti <me@hexile.me>
2021-06-26 12:19:34 +0200

    Fixed Java 8 compatibility
    I was building for Java 11 🤦

--- deobfuscator-helper/generate.sh
+++ deobfuscator-helper/generate.sh
@@ -2,7 +2,7 @@
 
 rm deobfuscator.jar
 rm -fr out
-javac -cp src/ $(find src -iname "*.java") -d out -classpath libs/commons-lang3-3.12.0.jar -classpath libs/commons-text-1.9.jar -Xlint:deprecation
+javac -cp src/ $(find src -iname "*.java") -d out -classpath libs/commons-lang3-3.12.0.jar -classpath libs/commons-text-1.9.jar -Xlint:deprecation -source 1.8 -target 1.8
 unzip -o libs/commons-text-1.9.jar -d out
 unzip -o libs/commons-lang3-3.12.0.jar -d out
 jar -cvmf manifest.mf deobfuscator.jar -C out .
